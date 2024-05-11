1434c2aa02d7e57b0bef0b2743dd00caa479f0a1
Terra Manning <terradmanning@gmail.com>
2024-05-11 10:53:00 -0600

    Complete the book tests

--- src/book.js
+++ src/book.js
@@ -1,12 +1,48 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return `The ${title}`
+}
+
+function buildMainCharacter(name, age, pronouns){
+  console.log({name}, {age}, {pronouns})
+  var character = {
+    name: name,
+    age: age,
+    pronouns: pronouns,
+  }
+  return character 
+}
+
+function saveReview(reviewStr, reviewsArr) {
+  if (!reviewsArr.includes(reviewStr)) {
+    reviewsArr.push(reviewStr)
+  }
+}
+
+function calculatePageCount(bookName) {
+  var pages = bookName.length * 20;
+  return pages
+}
+
+function writeBook (bookTitle, bookCharacter, type) {
+  var book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: calculatePageCount(bookTitle),
+    genre: type
+  }
+  return book
+}
+
+function editBook (someBook) {
+  someBook.pageCount = someBook.pageCount * .75;
 }
 
 module.exports = {
+
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  buildMainCharacter, 
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
